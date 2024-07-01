8e5b9dca6e5e4c3029961f5dfce76aae125501eb
DHANUSH247 <23203006@srcas.ac.in>
2024-07-01 11:58:00 +0530

    Create class-sample.java

---
+++ class-sample.java
@@ -0,0 +1,9 @@
+class sample{
+public static void main(String[]args)
+  {
+System.out.println("Hello");
+    System.out.println("World");
+    System.out.println("Hi");
+    System.out.println("welcome");
+  }
+}
